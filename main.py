2536e644305d8864ef86627920a5496a466796bf
jhenline <henlij@gmail.com>
2024-02-27 15:56:07 -0800

    Refactor to use CSV for data source

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
+import csv
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
-from predefined_zip_codes import predefined_zip_codes
 
 # Path to cache file used for faster performance after initial run
 cache_file_path = 'zip_code_cache.json'
@@ -46,6 +46,16 @@ def clean_zip_code(zip_code):
     return zip_code.split('-')[0]
 
 
+def read_zip_codes_from_csv(file_path):
+    zip_codes = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            zip_code = row['Zip'].strip()
+            zip_codes.append(clean_zip_code(zip_code))
+    return zip_codes
+
+
 def calculate_distance(coords1, coords2):
     try:
         return geodesic((coords1[0], coords1[1]), (coords2[0], coords2[1])).miles
@@ -62,9 +72,10 @@ def main():
 
     if home_coordinates:
         distances = []
-        for zip_code in predefined_zip_codes:
-            zip_code_cleaned = clean_zip_code(zip_code)
-            coordinates = get_coordinates(zip_code_cleaned, cache=zip_code_cache)
+        # Change here: Read zip codes from CSV instead of predefined list
+        mcplant_zip_codes = read_zip_codes_from_csv("mcplant_locations_feb_2024.csv")
+        for zip_code in mcplant_zip_codes:
+            coordinates = get_coordinates(zip_code, cache=zip_code_cache)
             if coordinates:
                 distance = calculate_distance(home_coordinates, coordinates)
                 if distance is not None:
